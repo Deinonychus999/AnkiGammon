a62d40b7082ee063d5228c3c2c2e21ad9f1e44ff
Deinonychus999 <frankcool999@hotmail.com>
2025-11-08 06:42:18 -0500

    Increase version to 1.0.4

--- ankigammon/__init__.py
+++ ankigammon/__init__.py
@@ -1,6 +1,6 @@
 """AnkiGammon: Convert eXtreme Gammon analysis into Anki flashcards."""
 
-__version__ = "1.0.3"
+__version__ = "1.0.4"
 
 from ankigammon.models import Decision, Move, Position, CubeState
 
